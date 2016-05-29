aba1cede6d13076f900048a6b0a9eef9ce26129c
bhaumik-choksi <bhaumik75@gmail.com>
2016-05-29 23:14:57 +0530

    Small changes in nav drawer

--- app/src/main/java/com/example/attendance/AttendanceActivity.java
+++ app/src/main/java/com/example/attendance/AttendanceActivity.java
@@ -2,12 +2,17 @@ package com.example.attendance;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class AttendanceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_attendance);
+        setContentView(R.layout.activity_attendance); //init
+
     }
 }
