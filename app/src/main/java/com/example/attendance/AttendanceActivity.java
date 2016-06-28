91a54d86eadb5ab5febf188264ea19db32d2509c
bhaumik-choksi <bhaumik75@gmail.com>
2016-06-28 22:30:35 +0530

    Login Activity added, not linked

--- app/src/main/java/com/example/attendance/AttendanceActivity.java
+++ app/src/main/java/com/example/attendance/AttendanceActivity.java
@@ -1,19 +1,28 @@
 package com.example.attendance;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class AttendanceActivity extends AppCompatActivity {
     public static int PRESENT = 0;
@@ -43,7 +52,9 @@ public class AttendanceActivity extends AppCompatActivity {
         //noinspection SimplifiableIfStatement
         if (id == R.id.mark_attendance_button) {
             //Toast.makeText(this,"Present : "+Integer.toString(PRESENT),Toast.LENGTH_SHORT).show();
-            Toast.makeText(this, loadJSONFromAsset(), Toast.LENGTH_SHORT).show();
+            //Toast.makeText(this, loadJSONFromAsset(), Toast.LENGTH_SHORT).show();
+            backgroundReceiveData brd = new backgroundReceiveData();
+            brd.execute();
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -76,5 +87,51 @@ public class AttendanceActivity extends AppCompatActivity {
     }
 
 
+    class backgroundReceiveData extends AsyncTask<Void, Void, String> {
+        String json_url;
+        String json_string;
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                URL url = new URL(json_url);
+                HttpURLConnection httpUrlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpUrlConnection.getInputStream();
+                BufferedReader bufferReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line;
+                while ((line = bufferReader.readLine()) != null) {
+                    result += line;
+                }
+                bufferReader.close();
+                inputStream.close();
+                httpUrlConnection.disconnect();
+                return result;
+            } catch (IOException u) {
+                return " url";
+            } catch (NullPointerException npe) {
+                return "nullpointer";
+            }
+            //return " this is bug";
+        }
+
+        @Override
+        protected void onPreExecute() {
+            json_url = "http://absattendance.esy.es/json.php";
+
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView dataToDisplay = (TextView) findViewById(R.id.demoTextView);
+            dataToDisplay.setText(result);
+            json_string = result;
+        }
+    }
 
 }
